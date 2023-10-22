b1ce5ea1245f09ea08c62d42b0034499c0eb95aa
nishida-sugiura <108458541+nishida-sugiura@users.noreply.github.com>
2023-10-22 16:44:32 +0900

    Add files via upload

---
+++ liff.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+    var liffId = "2001269046-RZ90vdYB";
+    initializeLiff(liffId);
+})
+
+function initializeLiff(liffId) {
+    liff
+        .init({
+            liffId: liffId
+        })
+        .then(() => {
+          // start to use LIFF's api
+          initializeApp();
+        })
+        .catch((err) => {
+            console.log('LIFF Initialization failed ', err);
+        });
+}
+
+function sendText(text) {
+    // sendMessages(text);
+// }
+
+// LINEトーク画面上でメッセージ送信
+// function sendMessages(text) {
+    liff.sendMessages([{
+        'type': 'text',
+        'text': text
+    }]).then(function () {  
+        liff.closeWindow();
+        
+    }).catch(function (error) {
+        window.alert('Failed to send message ' + error);
+    });
+}
